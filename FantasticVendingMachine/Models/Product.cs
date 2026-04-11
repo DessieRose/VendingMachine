7358a13ab252059086e1a2fd731049324ecd7445
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2026-04-11 11:19:18 +0200

    Remove explanatory comments from Product model

--- FantasticVendingMachine/Models/Product.cs
+++ FantasticVendingMachine/Models/Product.cs
@@ -2,18 +2,15 @@ namespace FantasticVendingMachine.Models;
 
 public class Product
 {
-    // Properties: These represent the "data" of a product
     public string Name { get; set; }
     public decimal Price { get; set; }
 
-    // Constructor: This allows you to create a product easily, e.g., new Product("Cola", 1.50m)
     public Product(string name, decimal price)
     {
         Name = name;
         Price = price;
     }
 
-    // A helper method to make it easy to print the product details later
     public override string ToString()
     {
         return $"{Name} - {Price:C}"; // :C formats the number as Currency (e.g., $1.50)
